9fd83d2d4df11beb69ea77e71fe226e1f56b64bf
fuckthecopyright <54515051+fuckthecopyright@users.noreply.github.com>
2020-10-26 06:24:07 -0400

    add gif generator and fonts

---
+++ src/gif_demo.py
@@ -0,0 +1,26 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+def create_gif(text, dir_path='.', filename='test.gif', imgsize=(600, 200), bgcolor=(255, 255, 255),
+           fgcolor=(102, 8, 116), fontfile='fonts\STXINGKA.ttf', fontsize=60, duration=200, loop=0):
+
+    img_seed = Image.new('RGB', imgsize, bgcolor)
+    font = ImageFont.truetype(fontfile, fontsize)
+
+    ctr = 1
+    images = []
+
+    for i in range(len(text)):
+        images.append(img_seed.copy())
+
+    for frame in images:
+        d = ImageDraw.Draw(frame)
+        d.text((10, 10), text[:ctr], fill=fgcolor, font=font)
+        ctr = ctr + 1
+
+    images[0].save(dir_path + '/' + filename,
+            save_all=True, append_images=images[1:], optimize=False, duration=duration, loop=loop)
+
+if __name__ == '__main__':
+    create_gif('字体放在fonts里\n换行只能手动')
