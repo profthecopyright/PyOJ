8933c6db7729ccb900ae716b316289d99d7a32f2
fuckthecopyright <54515051+fuckthecopyright@users.noreply.github.com>
2020-10-28 08:48:27 -0400

    添加成语词库

--- src/gif_demo.py
+++ src/gif_demo.py
@@ -3,12 +3,12 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 def create_gif(text, dir_path='.', filename='test.gif', imgsize=(600, 200), bgcolor=(255, 255, 255),
-           fgcolor=(102, 8, 116), fontfile='fonts\STXINGKA.ttf', fontsize=60, duration=200, loop=0):
+           fgcolor=(102, 8, 116), fontfile='fonts\STXINGKA.ttf', fontsize=60, duration=1000, loop=0):
 
     img_seed = Image.new('RGB', imgsize, bgcolor)
     font = ImageFont.truetype(fontfile, fontsize)
 
-    ctr = 1
+    ctr = 0
     images = [img_seed]
 
     for i in range(len(text)):
@@ -23,4 +23,4 @@ def create_gif(text, dir_path='.', filename='test.gif', imgsize=(600, 200), bgco
             save_all=True, append_images=images[1:], optimize=False, duration=duration, loop=loop)
 
 if __name__ == '__main__':
-    create_gif('字体放在fonts里\n换行只能手动')
+    create_gif('字体')
---
+++ src/scel_extractor.py
@@ -0,0 +1,125 @@
+# https://github.com/CQiang27/Spark_Python/blob/master/scel_TR_txt.ipynb
+# did not remove repetitive items
+
+import struct
+import os
+
+# 拼音表偏移，
+startPy = 0x1540
+
+# 汉语词组表偏移
+startChinese = 0x2628
+
+# 全局拼音表
+GPy_Table = {}
+
+# 解析结果
+# 元组(词频,拼音,中文词组)的列表
+GTable = []
+
+
+# 原始字节码转为字符串
+def byte2str(data):
+    pos = 0
+    str = ''
+    while pos < len(data):
+        c = chr(struct.unpack('H', bytes([data[pos], data[pos + 1]]))[0])
+        if c != chr(0):
+            str += c
+        pos += 2
+    return str
+
+
+# 获取拼音表
+def getPyTable(data):
+    data = data[4:]
+    pos = 0
+    while pos < len(data):
+        index = struct.unpack('H', bytes([data[pos], data[pos + 1]]))[0]
+        pos += 2
+        lenPy = struct.unpack('H', bytes([data[pos], data[pos + 1]]))[0]
+        pos += 2
+        py = byte2str(data[pos:pos + lenPy])
+        GPy_Table[index] = py
+        pos += lenPy
+
+
+# 获取一个词组的拼音
+def getWordPy(data):
+    pos = 0
+    ret = ''
+    while pos < len(data):
+        index = struct.unpack('H', bytes([data[pos], data[pos + 1]]))[0]
+        ret += GPy_Table[index]
+        pos += 2
+    return ret
+
+
+# 读取中文表
+def getChinese(data):
+    pos = 0
+    while pos < len(data):
+        # 同音词数量
+        same = struct.unpack('H', bytes([data[pos], data[pos + 1]]))[0]
+
+        # 拼音索引表长度
+        pos += 2
+        py_table_len = struct.unpack('H', bytes([data[pos], data[pos + 1]]))[0]
+
+        # 拼音索引表
+        pos += 2
+        py = getWordPy(data[pos: pos + py_table_len])
+
+        # 中文词组
+        pos += py_table_len
+        for i in range(same):
+            # 中文词组长度
+            c_len = struct.unpack('H', bytes([data[pos], data[pos + 1]]))[0]
+            # 中文词组
+            pos += 2
+            word = byte2str(data[pos: pos + c_len])
+            # 扩展数据长度
+            pos += c_len
+            ext_len = struct.unpack('H', bytes([data[pos], data[pos + 1]]))[0]
+            # 词频
+            pos += 2
+            count = struct.unpack('H', bytes([data[pos], data[pos + 1]]))[0]
+
+            # 保存
+            GTable.append((count, py, word))
+
+            # 到下个词的偏移位置
+            pos += ext_len
+
+
+def scel2txt(file_name):
+    # 分隔符
+    print('-' * 60)
+    # 读取文件
+    with open(file_name, 'rb') as f:
+        data = f.read()
+
+    print("词库名：", byte2str(data[0x130:0x338]))  # .encode('GB18030')
+    print("词库类型：", byte2str(data[0x338:0x540]))
+    print("描述信息：", byte2str(data[0x540:0xd40]))
+    print("词库示例：", byte2str(data[0xd40:startPy]))
+
+    getPyTable(data[startPy:startChinese])
+    getChinese(data[startChinese:])
+
+
+if __name__ == '__main__':
+
+    # scel所在文件夹路径
+    in_path = "../resources"
+
+    fin = [fname for fname in os.listdir(in_path) if fname[-5:] == ".scel"]
+    print(fin)
+    for f in fin:
+        f = os.path.join(in_path, f)
+        scel2txt(f)
+
+    f = open('coal_dict.txt', 'w')
+    for count, py, word in GTable:
+        f.write(str(count) + '\t\t\t' + py + '\t\t\t' + word + '\n')
+    f.close()
